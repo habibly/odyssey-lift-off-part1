5a725936d721e0cb72e500c02e34327736d5bb1c
habibly <ahmadhbb1995@gmail.com>
2022-07-02 15:03:37 +0500

    added query TRACKS, imported useQuery, using useQuery to print TRACKS data as JSON inside Layout

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  } 
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading, error, data} = useQuery(TRACKS);
+
+  if (loading) return 'Loading...';
+
+  if (error) return `ERROR! ${error.message}`;
+
+  return <Layout grid> {JSON.stringify(data)} </Layout>;
 };
 
 export default Tracks;
