516d90c1d401c7605f369fe530bbaf6039370ab1
habibly <ahmadhbb1995@gmail.com>
2022-07-02 14:15:00 +0500

    comments are good!

--- server/src/schema.js
+++ server/src/schema.js
@@ -9,16 +9,24 @@ type Query{
 "A track is group of Modules that teaches about a topic"
 type Track {
   id: ID!
+  "The track's title"
   title: String!
+  "The track's main Author"
   author: Author!
+  "The track's illustration to display in track card or track page detail"
   thumbnail: String
+  "The track's approximate length to complete, in minutes"
   length: Int
+  "The number of modules this track contains"
   modulesCount: Int
 }
 
+"Author of a complete Track or a Module"
 type Author {
   id: ID!
+  "Author's first and last name"
   name: String!
+  "Author's profile picture"
   photo: String
 }
 `
