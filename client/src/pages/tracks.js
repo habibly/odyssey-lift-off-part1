9dd55e6f81ac2a953f45d19288ff94500f14d6c7
habibly <ahmadhbb1995@gmail.com>
2022-07-02 15:23:13 +0500

    wrapped with QueryResult for nice loading animation along with error handling

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import { useQuery, gql } from '@apollo/client';
 import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 const TRACKS = gql`
   query getTracks {
@@ -26,15 +27,13 @@ const TRACKS = gql`
  */
 const Tracks = () => {
   const {loading, error, data} = useQuery(TRACKS);
-
-  if (loading) return 'Loading...';
-
-  if (error) return `ERROR! ${error.message}`;
-
   return (
-    <Layout grid> {data?.tracksForHome?.map((track) => (
-      <TrackCard key={track.id} track={track} />
-      ))} 
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+      </QueryResult>
     </Layout>
   );
 };
