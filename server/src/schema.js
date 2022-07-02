7e70ee84b2963b80dafe9e832fe53d5f04b208dd
habibly <ahmadhbb1995@gmail.com>
2022-07-02 14:44:53 +0500

    formatting is cool

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,33 +1,33 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
-type Query{
-  "Query to get tracks for the main page grid"
-  tracksForHome: [Track!]!
-}
+  type Query{
+    "Query to get tracks for the main page grid"
+    tracksForHome: [Track!]!
+  }
 
-"A track is group of Modules that teaches about a topic"
-type Track {
-  id: ID!
-  "The track's title"
-  title: String!
-  "The track's main Author"
-  author: Author!
-  "The track's illustration to display in track card or track page detail"
-  thumbnail: String
-  "The track's approximate length to complete, in minutes"
-  length: Int
-  "The number of modules this track contains"
-  modulesCount: Int
-}
+  "A track is group of Modules that teaches about a topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main Author"
+    author: Author!
+    "The track's illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
 
-"Author of a complete Track or a Module"
-type Author {
-  id: ID!
-  "Author's first and last name"
-  name: String!
-  "Author's profile picture"
-  photo: String
-}
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture"
+    photo: String
+  }
 `
 module.exports = typeDefs;
