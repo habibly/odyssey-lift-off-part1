de11a42cd300d2453cd17626806bc28d9d404833
habibly <ahmadhbb1995@gmail.com>
2022-07-02 18:07:10 +0500

    added new fields and Module type inside schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query{
     "Query to get tracks for the main page grid"
     tracksForHome: [Track!]!
+    track: Track
   }
 
   "A track is group of Modules that teaches about a topic"
@@ -19,8 +20,22 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The tracks description, can be in Markdown format"
+    description: String
+    "The number of time a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's Title"
+    title: String!
+    "The Modile's length in minutes"
+    length: Int
+  }
   "Author of a complete Track or a Module"
   type Author {
     id: ID!
