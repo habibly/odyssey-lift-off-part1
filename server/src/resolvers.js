11d34ee4c5725222bc2ae0527a92d19fcac348b2
habibly <ahmadhbb1995@gmail.com>
2022-07-02 17:56:49 +0500

    added addition fetch function for comparison on cached vs without cash response times

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -12,4 +12,27 @@ const resolvers = {
     }
   }
 }
+// fetch method
+// const resolvers = {
+//   Query: {
+//     tracksForHome: () => {
+//       // ...
+//     }
+//     tracksForHomeFetch: async () => {
+//       const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+//       const res = await fetch(`${baseUrl}/tracks`);
+//       return res.json();
+//     },
+//   },
+//   Track: {
+//     // using fetch instead of dataSources
+//     author: async ({ authorId }, _, { dataSources }) => {
+//       const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+//       const res = await fetch(`${baseUrl}/author/${authorId}`);
+//       return res.json();
+
+//       // return dataSources.trackAPI.getAuthor(authorId);
+//     },
+//   },
+// }
 module.exports = resolvers;
